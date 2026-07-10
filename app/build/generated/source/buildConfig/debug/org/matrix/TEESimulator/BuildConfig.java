4d16bf98d468bd38d78d9c6ad187ffbe4e31ab78
clcwpwqi <clcwpwqi@users.noreply.github.com>
2026-07-10 15:23:38 +0000

    feat: Build, Release, and Document GitHub Release
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ app/build/generated/source/buildConfig/debug/org/matrix/TEESimulator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.matrix.TEESimulator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.matrix.TEESimulator";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 292;
+  public static final String VERSION_NAME = "v6.0.1";
+}
---
+++ app/build/generated/source/buildConfig/release/org/matrix/TEESimulator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.matrix.TEESimulator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.matrix.TEESimulator";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 292;
+  public static final String VERSION_NAME = "v6.0.1";
+}
